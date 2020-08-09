3d3fd2880a69728613a30b0d109939afde9ffe23
Andidev30 <aandi30082001@gmail.com>
2020-08-10 05:30:10 +0700

    initial branch dev-

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="bg-warning text-white p-3 text-center">
-            Copyright @ 2020 Dewe Tour - Your Name - NIS. All Rights reserved
+            Copyright @ 2020 Dewe Tour - Andi - NIS. All Rights reserved
         </div>
     )
 }
